82828b17387caae9bcfcff15f5352b75e039fb67
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2025-03-19 23:07:26 -0300

    add MovieCard

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+
+function MovieCard({movie}) {
+
+    function handleFavorite(movie) {
+        console.log("Favorite movie", movie);
+    }
+
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title} />
+                <div className="movie-overlay">
+                <button className="favorite-btn" onClick={() => handleFavorite(movie)}>Like
+                    👍
+                </button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+            </div>
+        </div>
+    );
+}
+
+export default MovieCard;
